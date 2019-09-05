4e3876e01d6c8d1c6dc3f8d61b1d4898cb042ac7
omega <erez@guesty.com>
2019-09-05 09:57:40 +0300

    camera, map

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,12 +1,95 @@
-import React from 'react';
-import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity, CameraRoll, StyleSheet } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
 
-export default function CameraScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>hellow world</Text>
-    </View>
-  );
+export default class CameraScreen extends Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+  cameraRef = null;
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  snap = async () => {
+    let photo;
+    if (this.cameraRef) {
+      photo = await this.cameraRef.takePictureAsync();
+    }
+    console.log(photo);
+    try {
+      let saveResult = await CameraRoll.saveToCameraRoll(photo.uri, 'photo');
+      console.log(saveResult);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View/>;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera
+            style={{ flex: 1 }}
+            type={this.state.type}
+            ref={ref => {
+              this.cameraRef = ref;
+            }}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type:
+                      this.state.type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}
+                  Flip{' '}
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                  backgroundColor: 'red',
+                }}
+                onPress={this.snap}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  Snap2
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
 }
 
 CameraScreen.navigationOptions = {
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
+import MapView from 'react-native-maps';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 export default class MapScreen extends Component {
+  state = {
+    location: null,
+    errorMessage: null,
+  };
+
+  async componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage:
+          'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      await this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+
   render() {
-    return (<View><Text>MapScreen</Text></View>);
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        showsUserLocation={true}
+      />
+    );
   }
 }
 
